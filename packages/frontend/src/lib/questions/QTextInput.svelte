c2a02f0fbda79ca64277373c4e1d02f987ca0017
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-07 18:27:58 +0000

    add QTextInput and put it on component demo page

---
+++ packages/frontend/src/lib/questions/QTextInput.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let editmode = false;
+	export let multiline = false;
+	let response = '';
+	export let prompt = '';
+</script>
+
+<div>
+	<div>
+		{#if editmode}
+			<input bind:value={prompt} />
+		{:else}
+			<span>{prompt}</span>
+		{/if}
+	</div>
+
+	{#if multiline}
+		<textarea bind:value={response} disabled={editmode} />
+	{:else}
+		<input bind:value={response} disabled={editmode} />
+	{/if}
+</div>
--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+	import QTextInput from '../lib/questions/QTextInput.svelte';
+
+	let editmode = false;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>Component Demo</h2>
+
+<div>
+	<input type="checkbox" id="editmode" bind:checked={editmode} />
+	<label for="editmode">Survey Edit Mode</label>
+</div>
+
+<QTextInput prompt="What is your name?" {editmode} />
+<QTextInput prompt="Describe your shoes." multiline={true} {editmode} />
