7edd2e42aed2bcdb28f3d7b663be946b7068ada3
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-09 22:57:52 +0000

    add QRating

---
+++ packages/frontend/src/lib/questions/QRating.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let editmode = false;
+	let response = 0;
+	export let prompt: string;
+	export let description: string;
+	export let max_rating = 10;
+</script>
+
+<div>
+	<div>
+		{#if editmode}
+			<span>On a scale of 1- <input bind:value={max_rating} type="number" /> ...</span>
+		{:else}
+			<span>On a scale of 1-{max_rating}...</span>
+		{/if}
+	</div>
+
+	<div>
+		{#if editmode}
+			<input bind:value={prompt} />
+		{:else}
+			<span>{prompt}</span>
+		{/if}
+	</div>
+
+	<div>
+		{#if editmode}
+			<input bind:value={description} />
+		{:else}
+			<span>{description}</span>
+		{/if}
+	</div>
+
+	<div>
+		<input bind:value={response} type="number" disabled={editmode} />
+	</div>
+</div>
