ed9c8f31ad1232d8c658fc106c04f1abf5972ddf
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-09 01:17:14 +0000

    update migration

--- crates/api_server/migrations/2023-02-08-204138_create_users/up.sql
+++ crates/api_server/migrations/2023-02-08-204138_create_users/up.sql
@@ -2,6 +2,6 @@ CREATE TABLE users (
 	id INTEGER PRIMARY KEY,
 	username TEXT NOT NULL,
 	password TEXT NOT NULL,
-	created_at TEXT NOT NULL,
-	updated_at TEXT NOT NULL
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
