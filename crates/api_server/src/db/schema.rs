0254999a5fe4a5676651d6c0ec5a272075e0e30e
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-11 01:16:15 +0000

    update generated code

--- crates/api_server/src/db/schema.rs
+++ crates/api_server/src/db/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    surveys (id) {
+        id -> Int4,
+        title -> Text,
+        description -> Text,
+        published -> Bool,
+        owner_id -> Int4,
+        questions -> Jsonb,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         username -> Text,
@@ -9,3 +20,10 @@ diesel::table! {
         updated_at -> Timestamp,
     }
 }
+
+diesel::joinable!(surveys -> users (owner_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    surveys,
+    users,
+);
