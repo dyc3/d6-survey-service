162a21740809e355758be81c1d6deba2a5d416d6
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-11 00:53:33 +0000

    make questions public

--- crates/api_server/src/questions.rs
+++ crates/api_server/src/questions.rs
@@ -1,16 +1,25 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[typeshare]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SurveyQuestion {
+    pub uuid: Uuid,
+    pub required: bool,
+    pub question: Question,
+}
 
 #[typeshare]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type", content = "content")]
-enum Question {
+pub enum Question {
     Text(QText),
     Rating(QRating),
 }
 
 #[typeshare]
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct QText {
+pub struct QText {
     prompt: String,
     description: String,
     multiline: bool,
@@ -19,7 +28,7 @@ struct QText {
 /// Represents a question like "On a scale of 1 to N, how do you feel about X?"
 #[typeshare]
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct QRating {
+pub struct QRating {
     prompt: String,
     description: String,
     max_rating: u8,
