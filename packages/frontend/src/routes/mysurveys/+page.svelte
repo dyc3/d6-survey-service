9ae580984ea150163467647a4bc5c4fa016bc3a4
Carson McManus <dyc3@users.noreply.github.com>
2023-02-15 14:02:35 -0500

    list survey page (#33)
    - add mysurveys page
    - finish mock up of survey list page
    
    Non functional, but it'll be pretty trivial to hook it up.
    
    
    ![image](https://user-images.githubusercontent.com/1808807/219126193-a70b446f-91dc-42ba-a26e-c6eb9df9a04e.png)
    
    closes #12

---
+++ packages/frontend/src/routes/mysurveys/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import Button from '../../lib/ui/Button.svelte';
+	import TextBox from '../../lib/ui/TextBox.svelte';
+</script>
+
+<div class="toolbar">
+	<h1>My Surveys</h1>
+	<Button kind="primary" size="large">Create Survey</Button>
+</div>
+
+<table class="container">
+	<thead class="header">
+		<th class="name">Name</th>
+		<th class="published">Published</th>
+		<th class="share-link">Share Link</th>
+		<th class="actions">Actions</th>
+	</thead>
+	<tbody>
+		<tr class="survey">
+			<td class="name">Survey 1</td>
+			<!-- TODO: replace with check box-->
+			<td class="published">Yes</td>
+			<!-- TODO: make this read only-->
+			<td class="share-link"><TextBox value="https://www.survey.com/123" /></td>
+			<td class="actions">
+				<Button>Edit</Button>
+				<Button>Delete</Button>
+			</td>
+		</tr>
+		<tr class="survey">
+			<td class="name">Survey 2</td>
+			<td class="published">Yes</td>
+			<td class="share-link"><TextBox value="https://www.survey.com/123" /></td>
+			<td class="actions">
+				<Button>Edit</Button>
+				<Button>Delete</Button>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<style lang="scss">
+	.container {
+		width: 100%;
+	}
+	.header {
+		align-items: center;
+		text-align: left;
+	}
+	.survey {
+		align-items: center;
+	}
+	.published {
+		text-align: center;
+	}
+
+	.toolbar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
