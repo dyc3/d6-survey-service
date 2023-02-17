c831b5ef5debf7965ffdb39d0fde8cb16926529d
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-17 16:20:05 +0000

    format

--- crates/api_server/benches/surveys.rs
+++ crates/api_server/benches/surveys.rs
@@ -1,10 +1,9 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use api_server::questions::{SurveyQuestion, QText, QRating, QMultipleChoice};
-use api_server::test_helpers::*;
 use api_server::db::models::SurveyPatch;
+use api_server::questions::{QMultipleChoice, QRating, QText, SurveyQuestion};
+use api_server::test_helpers::*;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rocket::local::blocking::Client;
 use rocket::uri;
-
 
 fn get_survey(c: &mut Criterion) {
     let questions = vec![
@@ -12,38 +11,49 @@ fn get_survey(c: &mut Criterion) {
             prompt: "What is your name?".to_string(),
             description: "Please enter your name".to_string(),
             multiline: false,
-        }.into(),
+        }
+        .into(),
         QRating {
             prompt: "Rate the foobar".to_string(),
             description: "1 is the worst, 5 is the best".to_string(),
-            max_rating: 5
-        }.into(),
+            max_rating: 5,
+        }
+        .into(),
         QMultipleChoice {
             prompt: "Pick one".to_string(),
             description: "yes".to_string(),
-        }.into(),
+        }
+        .into(),
         QText {
             prompt: "What is your name?".to_string(),
             description: "Please enter your name".to_string(),
             multiline: true,
-        }.into(),
+        }
+        .into(),
         QRating {
             prompt: "Rate the foobar".to_string(),
             description: "1 is the worst, 5 is the best".to_string(),
-            max_rating: 5
-        }.into(),
-    ].into_iter().map(|q| {
-        SurveyQuestion {
-            uuid: uuid::Uuid::new_v4(),
-            required: false,
-            question: q,
+            max_rating: 5,
         }
-    }).collect::<Vec<_>>();
+        .into(),
+    ]
+    .into_iter()
+    .map(|q| SurveyQuestion {
+        uuid: uuid::Uuid::new_v4(),
+        required: false,
+        question: q,
+    })
+    .collect::<Vec<_>>();
 
     let db_name = create_db_for_tests();
     let client = Client::untracked(bench_rocket(&db_name)).expect("valid rocket instance");
     let token = create_test_user(&client);
-    fn make_test_survey(client: &Client, token: &String, questions: &Vec<SurveyQuestion>, mut amount: usize) -> i32 {
+    fn make_test_survey(
+        client: &Client,
+        token: &String,
+        questions: &Vec<SurveyQuestion>,
+        mut amount: usize,
+    ) -> i32 {
         let survey_id = make_survey(&client, &token);
         let mut q = vec![];
         while amount > questions.len() {
@@ -70,11 +80,14 @@ fn get_survey(c: &mut Criterion) {
 
     for survey_size in [2, 4, 8, 16, 32].iter() {
         let survey_id = make_test_survey(&client, &token, &questions, *survey_size);
-        c.bench_function(&format!("get survey: {} questions", survey_size), |b| b.iter(|| {
-            let resp = client.get(uri!("/api", api_server::survey::get_survey(survey_id)).to_string())
-                .dispatch();
-            black_box(resp);
-        }));
+        c.bench_function(&format!("get survey: {} questions", survey_size), |b| {
+            b.iter(|| {
+                let resp = client
+                    .get(uri!("/api", api_server::survey::get_survey(survey_id)).to_string())
+                    .dispatch();
+                black_box(resp);
+            })
+        });
     }
 
     drop_test_db(db_name);
@@ -86,38 +99,49 @@ fn patch_survey(c: &mut Criterion) {
             prompt: "What is your name?".to_string(),
             description: "Please enter your name".to_string(),
             multiline: false,
-        }.into(),
+        }
+        .into(),
         QRating {
             prompt: "Rate the foobar".to_string(),
             description: "1 is the worst, 5 is the best".to_string(),
-            max_rating: 5
-        }.into(),
+            max_rating: 5,
+        }
+        .into(),
         QMultipleChoice {
             prompt: "Pick one".to_string(),
             description: "yes".to_string(),
-        }.into(),
+        }
+        .into(),
         QText {
             prompt: "What is your name?".to_string(),
             description: "Please enter your name".to_string(),
             multiline: true,
-        }.into(),
+        }
+        .into(),
         QRating {
             prompt: "Rate the foobar".to_string(),
             description: "1 is the worst, 5 is the best".to_string(),
-            max_rating: 5
-        }.into(),
-    ].into_iter().map(|q| {
-        SurveyQuestion {
-            uuid: uuid::Uuid::new_v4(),
-            required: false,
-            question: q,
+            max_rating: 5,
         }
-    }).collect::<Vec<_>>();
+        .into(),
+    ]
+    .into_iter()
+    .map(|q| SurveyQuestion {
+        uuid: uuid::Uuid::new_v4(),
+        required: false,
+        question: q,
+    })
+    .collect::<Vec<_>>();
 
     let db_name = create_db_for_tests();
     let client = Client::untracked(bench_rocket(&db_name)).expect("valid rocket instance");
     let token = create_test_user(&client);
-    fn make_test_survey(client: &Client, token: &String, questions: &Vec<SurveyQuestion>, mut amount: usize) -> (i32, Vec<u8>) {
+    fn make_test_survey(
+        client: &Client,
+        token: &String,
+        questions: &Vec<SurveyQuestion>,
+        mut amount: usize,
+    ) -> (i32, Vec<u8>) {
         let survey_id = make_survey(&client, &token);
         let mut q = vec![];
         while amount > questions.len() {
@@ -139,15 +163,17 @@ fn patch_survey(c: &mut Criterion) {
     for survey_size in [2, 4, 8, 16, 32].iter() {
         let (survey_id, body) = make_test_survey(&client, &token, &questions, *survey_size);
 
-        c.bench_function(&format!("patch survey: {} questions", survey_size), |b| b.iter(|| {
-            let resp = client.get(uri!("/api", api_server::survey::edit_survey(survey_id))
-            .to_string())
-            .header(rocket::http::ContentType::JSON)
-            .header(rocket::http::Header::new("Authorization", token.clone()))
-            .body(body.clone())
-                .dispatch();
-            black_box(resp);
-        }));
+        c.bench_function(&format!("patch survey: {} questions", survey_size), |b| {
+            b.iter(|| {
+                let resp = client
+                    .get(uri!("/api", api_server::survey::edit_survey(survey_id)).to_string())
+                    .header(rocket::http::ContentType::JSON)
+                    .header(rocket::http::Header::new("Authorization", token.clone()))
+                    .body(body.clone())
+                    .dispatch();
+                black_box(resp);
+            })
+        });
     }
 
     drop_test_db(db_name);
