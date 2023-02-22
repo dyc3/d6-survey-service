b7f08f69239d37ae3ef7f64433ffd98e0e0e18a9
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-22 21:20:30 +0000

    add migration for creating responses table

---
+++ crates/api_server/migrations/2023-02-22-211018_create_responses/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE responses (
+	survey_id SERIAL NOT NULL REFERENCES surveys (id) ON DELETE CASCADE,
+	responder_uuid UUID PRIMARY KEY,
+	content JSONB NOT NULL DEFAULT '{}'::JSONB,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	UNIQUE(survey_id, responder_uuid)
+);
