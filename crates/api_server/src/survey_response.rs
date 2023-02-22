0ecf9628474b87313193ed541ccf68d0d8ba8068
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-22 21:31:25 +0000

    update api endpoints

--- crates/api_server/src/survey_response.rs
+++ crates/api_server/src/survey_response.rs
@@ -1,13 +1,14 @@
 use rocket::serde::json::Json;
+use uuid::Uuid;
 
-use crate::questions::SurveyResponse;
+use crate::db::models::SurveyResponses;
 
 #[post("/survey/<survey_id>/respond", data = "<survey_response>")]
-fn create_survey_response(survey_id: i32, survey_response: Json<SurveyResponse>) {
+fn create_survey_response(survey_id: i32, survey_response: Json<SurveyResponses>) {
 
 }
 
-#[patch("/survey/<survey_id>/respond", data = "<survey_response>")]
-fn edit_survey_response(survey_id: i32, survey_response: Json<SurveyResponse>) {
+#[patch("/survey/<survey_id>/respond?responder=<responder_uuid>", data = "<survey_response>")]
+fn edit_survey_response(survey_id: i32, survey_response: Json<SurveyResponses>, responder_uuid: Uuid) {
 
 }
