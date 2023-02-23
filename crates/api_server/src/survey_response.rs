86a23a2a08eed2976bc02e60621896c6d8438829
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-23 00:05:07 +0000

    impl create survey response endpoint

--- crates/api_server/src/survey_response.rs
+++ crates/api_server/src/survey_response.rs
@@ -1,14 +1,82 @@
-use rocket::serde::json::Json;
+use diesel::prelude::*;
+use rocket::{http::Status, serde::json::Json};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use uuid::Uuid;
 
-use crate::db::models::SurveyResponses;
+use crate::{
+    api::ApiErrorResponse,
+    db::{
+        models::{NewSurveyResponse, SurveyResponses},
+        Storage,
+    },
+};
+
+#[typeshare]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseAccepted {
+    responder_uuid: Uuid,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Error)]
+pub enum SurveyResponseError {
+    #[error("Survey not found")]
+    SurveyNotFound,
+    #[error("Survey responder not found")]
+    ResponderNotFound,
+    #[error("Validation failed")]
+    ValidationFailed,
+    #[error("Unknown error")]
+    Unknown,
+}
+
+impl From<SurveyResponseError> for ApiErrorResponse<SurveyResponseError> {
+    fn from(value: SurveyResponseError) -> Self {
+        let status = match &value {
+            SurveyResponseError::SurveyNotFound => Status::NotFound,
+            SurveyResponseError::ResponderNotFound => Status::NotFound,
+            SurveyResponseError::ValidationFailed => Status::BadRequest,
+            SurveyResponseError::Unknown => Status::InternalServerError,
+        };
+        ApiErrorResponse {
+            status,
+            message: value,
+        }
+    }
+}
 
 #[post("/survey/<survey_id>/respond", data = "<survey_response>")]
-fn create_survey_response(survey_id: i32, survey_response: Json<SurveyResponses>) {
+async fn create_survey_response(
+    db: Storage,
+    survey_id: i32,
+    survey_response: Json<SurveyResponses>,
+) -> Result<Json<ResponseAccepted>, ApiErrorResponse<SurveyResponseError>> {
+    let uuid = db
+        .run(move |conn| {
+            let survey_response = survey_response.into_inner();
+            let uuid = Uuid::new_v4();
+            let new_survey_response = NewSurveyResponse {
+                survey_id,
+                responder_uuid: uuid,
+                content: survey_response,
+            };
+            diesel::insert_into(crate::db::schema::responses::table)
+                .values(&new_survey_response)
+                .execute(conn)
+                .map(|_| uuid)
+        })
+        .await
+        .map_err(|e| {
+            error!("{e:?}");
+            SurveyResponseError::Unknown
+        })?;
 
+    Ok(Json(ResponseAccepted {
+        responder_uuid: uuid,
+    }))
 }
 
-#[patch("/survey/<survey_id>/respond?responder=<responder_uuid>", data = "<survey_response>")]
-fn edit_survey_response(survey_id: i32, survey_response: Json<SurveyResponses>, responder_uuid: Uuid) {
+// #[patch("/survey/<survey_id>/respond?responder=<responder_uuid>", data = "<survey_response>")]
+// async fn edit_survey_response(db: Storage, survey_id: i32, survey_response: Json<SurveyResponses>, responder_uuid: Uuid) {
 
-}
+// }
