1bdff16574e3f15e89bfe967c8807cc6cf7adb2e
zutingc <102315862+zutingc@users.noreply.github.com>
2023-02-25 08:25:34 -0500

    Add login page (#60)
    - initial test
    - frontend framework
    - add login page
    
    closes #32 
    closes #11
    
    ---------
    
    Co-authored-by: Carson McManus <carson.mcmanus1@gmail.com>

---
+++ packages/frontend/src/lib/ui/Main.svelte
@@ -0,0 +1,26 @@
+
+<div class="mainLayout">
+	<slot>
+	</slot>
+</div>
+<style lang="scss">
+	.mainLayout {
+		// display: flex;
+		justify-content: center;
+		align-items: center;
+
+		padding: 20px 30px 20px 30px;
+        // margin: 100px 200px 100px 200px; // change to center later?
+		margin: auto;
+
+		width: max-content;
+
+		border: #000 3px solid;
+		border-radius: 4px;
+		background-color: #fff;
+
+        line-height: 2;
+        color: black;
+		font-family: 'DM Sans';
+	}
+</style>
---
+++ packages/frontend/src/routes/login/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import Main from '$lib/ui/Main.svelte';
+	import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@rgossiaux/svelte-headlessui';
+	import Button from '$lib/ui/Button.svelte';
+	import TextBox from '$lib/ui/TextBox.svelte';
+	import { loginUser, registerUser } from '$lib/api';
+	import { jwt } from '../../stores';
+	import { goto } from '$app/navigation';
+	import type { UserToken } from '$lib/common';
+	import type { ApiResponse } from '$lib/api';
+
+	let username = '';
+	let password = '';
+	let response = '';
+
+	function handleLogin(resp: ApiResponse<UserToken>) {
+		if (resp.ok) {
+			response = 'Successfully logged in.';
+			jwt.login(resp.value.token);
+			goto('/mysurveys');
+		} else {
+			console.error(resp.error);
+			response = resp.error.message;
+		}
+	}
+
+	async function doLogin() {
+		handleLogin(await loginUser({ username, password }));
+	}
+
+	async function doRegister() {
+		handleLogin(await registerUser({ username, password }));
+	}
+</script>
+
+<Main>
+	<TabGroup>
+		Welcome to the Survey App!
+		<TabList>
+			<Tab>Log In</Tab>
+			<Tab>Register</Tab>
+		</TabList>
+		<TabPanels>
+			<TabPanel style="width: max-content; margin: auto">
+				Log in <br />
+				<TextBox placeholder="Username" bind:value={username} /> <br />
+				<TextBox placeholder="Password" bind:value={password} /> <br />
+				<Button kind="primary" on:click={doLogin}>Submit</Button>
+				<span>{response}</span>
+			</TabPanel>
+			<TabPanel style="width: max-content; margin: auto">
+				Create a New User <br />
+				<TextBox placeholder="New Username" bind:value={username} /> <br />
+				<TextBox placeholder="New Password" bind:value={password} /> <br />
+				<Button kind="primary" on:click={doRegister}>Submit</Button>
+				<span>{response}</span>
+			</TabPanel>
+		</TabPanels>
+	</TabGroup>
+</Main>
