80b4ada42097886848ee3e358d157fb1f6b78476
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-25 18:23:16 +0000

    speed up tests and cover all needed cases

--- packages/frontend/tests/user-login.test.ts
+++ packages/frontend/tests/user-login.test.ts
@@ -1,23 +1,25 @@
 import { expect, test } from '@playwright/test';
 import crypto from 'crypto';
 
-test('should register user and go to mysurveys', async ({ page }, testInfo) => {
+test.afterEach(async ({ page }) => {
+	await page.evaluate(() => localStorage.clear());
+});
+
+test('should register user or log in user and go to mysurveys', async ({ page }, testInfo) => {
+	const username = `testuser-${crypto.randomUUID()}`;
+	const password = 'pass';
+
 	await page.goto('/login');
 	await page.getByRole("tab", { name: "Register" }).click();
-	await page.fill('input[name="username"]', `testuser-${crypto.randomUUID()}`);
-	await page.fill('input[name="password"]', 'pass');
+	await page.fill('input[name="username"]', username);
+	await page.fill('input[name="password"]', password);
 	await page.getByRole("button", { name: "Submit" }).click();
 	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: "visible" });
 	// await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
-});
+	let token = await page.evaluate(() => localStorage.getItem('token'));
+	expect(token).toBeTruthy();
 
-test('should log in existing user and go to mysurveys', async ({ page, request }, testInfo) => {
-	const username = `testuser-${crypto.randomUUID()}`;
-	const password = 'pass';
-	const register = await request.post('/api/user/register', {
-		data: { username, password }
-	});
-	expect(register.ok()).toBe(true);
+	await page.evaluate(() => localStorage.clear());
 
 	await page.goto('/login');
 	await page.getByRole("tab", { name: "Log In" }).click();
@@ -25,5 +27,19 @@ test('should log in existing user and go to mysurveys', async ({ page, request }
 	await page.fill('input[name="password"]', password);
 	await page.getByRole("button", { name: "Submit" }).click();
 	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: "visible" });
+	token = await page.evaluate(() => localStorage.getItem('token'));
+	expect(token).toBeTruthy();
+});
+
+test('should not register user and should not log in', async ({ page }, testInfo) => {
+	await page.goto('/login');
+	await page.getByRole("tab", { name: "Register" }).click();
+	await page.getByRole("button", { name: "Submit" }).click();
+	await page.getByText("InvalidCredentials").waitFor({ state: "visible" });
+	// await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
+
+	await page.getByRole("tab", { name: "Log In" }).click();
+	await page.getByRole("button", { name: "Submit" }).click();
+	await page.getByText("InvalidCredentials").waitFor({ state: "visible" });
 	// await page.screenshot({ path: `screenshots/${testInfo.title}.png` });
 });
