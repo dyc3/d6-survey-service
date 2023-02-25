8eaddf819ede75bf63c2251e82abe934a9f19af1
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-25 15:37:01 +0000

    format

--- crates/api_server/src/db/schema.rs
+++ crates/api_server/src/db/schema.rs
@@ -25,7 +25,4 @@ diesel::table! {
 
 diesel::joinable!(surveys -> users (owner_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    surveys,
-    users,
-);
+diesel::allow_tables_to_appear_in_same_query!(surveys, users,);
