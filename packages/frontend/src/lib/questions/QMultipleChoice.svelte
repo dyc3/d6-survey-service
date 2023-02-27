6fd39e8181dca1226a1413932c21a24776fa905e
Carson McManus <carson.mcmanus1@gmail.com>
2023-02-27 22:03:08 +0000

    make QMultipleChoice editable

--- packages/frontend/src/lib/questions/QMultipleChoice.svelte
+++ packages/frontend/src/lib/questions/QMultipleChoice.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    export let editmode = false;
-    export let prompt: string;
-    export let description: string;
-    import Button from "$lib/ui/Button.svelte";
-    import TextBox from "$lib/ui/TextBox.svelte";
+	import type { Choice } from '$lib/common';
+	import Button from '$lib/ui/Button.svelte';
+	import TextBox from '$lib/ui/TextBox.svelte';
 
-    let options = ["Option 1", "Option 2"];
+	export let editmode = false;
+	export let prompt: string;
+	export let description: string;
+	export let choices: Choice[] = [];
 
-    function addOptions(){
-        // work in progress
-        // options = [...options, "Option " + options.length + 1];
-    }
-
+	function addChoice() {
+		choices = [...choices, { uuid: crypto.randomUUID(), text: '' }];
+	}
 </script>
 
 <div>
@@ -31,30 +30,24 @@
 		{/if}
 	</div>
 
-	<div>
-        {#if editmode}
-            <TextBox bind:value={options[0]} />
-        {:else}
-            <Button>{options[0]}</Button>
-        {/if}
-    </div>
-    <div>
-        {#if editmode}
-            <TextBox bind:value={options[1]} />
-        {:else}
-            <Button>{options[1]}</Button>
-        {/if}
-    </div>
-    <div>
-        
-    </div>
-    <div>
-        {#if editmode}
-        <span>+</span>
-        {:else}
-        <Button on:click={addOptions}>
-        +
-        </Button>
-        {/if}
-    </div>
+	<div class="choices">
+		<!-- TODO: make this a button group, see #75 -->
+		{#each choices as choice}
+			{#if editmode}
+				<TextBox bind:value={choice.text} placeholder="Enter text..." />
+			{:else}
+				<Button>{choice.text}</Button>
+			{/if}
+		{/each}
+		{#if editmode}
+			<Button on:click={addChoice}>+</Button>
+		{/if}
+	</div>
 </div>
+
+<style lang="scss">
+	.choices {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
