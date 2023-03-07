9af8e72d416abd3dadefdd24d38f48ded109296a
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-07 21:11:25 +0000

    change ApiOkCacheableResource to enum

--- crates/api_server/src/api.rs
+++ crates/api_server/src/api.rs
@@ -8,7 +8,10 @@ use serde::Serialize;
 use crate::cache::Cacheable;
 
 #[derive(Debug)]
-pub struct ApiOkCacheableResource<T>(pub T);
+pub enum ApiOkCacheableResource<T> {
+    Ok(T),
+    NotModified,
+}
 
 #[rocket::async_trait]
 impl<'r, T> Responder<'r, 'static> for ApiOkCacheableResource<T>
@@ -16,17 +19,25 @@ where
     T: Serialize + Cacheable,
 {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
-        let body = serde_json::to_string(&self.0).map_err(|e| {
-            error!("could not serialize response: {e:?}");
-            Status::InternalServerError
-        })?;
+        match self {
+            ApiOkCacheableResource::Ok(t) => {
+                let body = serde_json::to_string(&t).map_err(|e| {
+                    error!("could not serialize response: {e:?}");
+                    Status::InternalServerError
+                })?;
 
-        let mut resp = Response::build();
-        resp.header(ContentType::JSON);
-        self.0.last_modified_header().map(|h| resp.header(Header::new("Last-Modified", h)));
-        self.0.etag_header().map(|h| resp.header(Header::new("ETag", h)));
-        resp.sized_body(body.len(), Cursor::new(body))
-            .ok()
+                let mut resp = Response::build();
+                resp.header(ContentType::JSON);
+                t.last_modified_header().map(|h| resp.header(Header::new("Last-Modified", h)));
+                t.etag_header().map(|h| resp.header(Header::new("ETag", h)));
+                resp.sized_body(body.len(), Cursor::new(body))
+                    .ok()
+            },
+            ApiOkCacheableResource::NotModified => Response::build()
+                .status(Status::NotModified)
+                .ok(),
+        }
+
     }
 }
 
