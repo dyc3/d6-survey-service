3288b3b08722a015d9c7c601ec9ece32d0a6af82
invissy <72313458+invissy@users.noreply.github.com>
2023-03-08 19:11:04 +0000

    Added UI components to the creation page

--- packages/frontend/src/routes/survey/[slug]/edit/+page.svelte
+++ packages/frontend/src/routes/survey/[slug]/edit/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { Question, SurveyQuestions } from '$lib/common';
 	import QContainer from '$lib/QContainer.svelte';
+	
+	import Button from '$lib/ui/Button.svelte';
+	import TextBox from '$lib/ui/TextBox.svelte';
 
 	let questions: SurveyQuestions = [];
 
@@ -59,3 +62,18 @@
 		questions = questions.filter((q) => q.uuid !== uuid);
 	}
 </script>
+
+<h1>Sample Survey Title</h1>
+<h2>Editing</h2>
+<Button>View Results</Button>
+
+<div>
+	<!--Replace these-->
+<TextBox placeholder='Survey Title'></TextBox>
+<TextBox placeholder='Survey Description'></TextBox>
+
+
+
+<Button></Button>
+
+</div>
