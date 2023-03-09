a118b4038b52e83141fef7c7dc6875d88d662a3c
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-09 14:16:35 +0000

    avoid db query if not needed

--- crates/api_server/src/survey_response.rs
+++ crates/api_server/src/survey_response.rs
@@ -114,20 +114,20 @@ pub async fn edit_survey_response(
     responder: Uuid,
     race_check: Option<RaceCheck>,
 ) -> Result<(), ApiErrorResponse<SurveyResponseError>> {
-    let old_response = db
-        .run(move |conn| {
-            crate::db::schema::responses::table
-                .filter(crate::db::schema::responses::survey_id.eq(survey_id))
-                .filter(crate::db::schema::responses::responder_uuid.eq(responder))
-                .first::<SurveyResponse>(conn)
-        })
-        .await
-        .map_err(|e| {
-            error!("{e:?}");
-            SurveyResponseError::Unknown
-        })?;
-
     if let Some(race_check) = race_check {
+        let old_response = db
+            .run(move |conn| {
+                crate::db::schema::responses::table
+                    .filter(crate::db::schema::responses::survey_id.eq(survey_id))
+                    .filter(crate::db::schema::responses::responder_uuid.eq(responder))
+                    .first::<SurveyResponse>(conn)
+            })
+            .await
+            .map_err(|e| {
+                error!("{e:?}");
+                SurveyResponseError::Unknown
+            })?;
+
         if !old_response.has_no_mid_air_collision(race_check) {
             return Err(SurveyResponseError::RaceError.into());
         }
