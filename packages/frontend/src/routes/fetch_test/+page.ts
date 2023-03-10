184eba2c4866482153e4be381203d08310e8d4da
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-10 16:35:54 +0000

    disable ssr for authenticated requests

--- packages/frontend/src/routes/fetch_test/+page.ts
+++ packages/frontend/src/routes/fetch_test/+page.ts
@@ -1,6 +1,13 @@
-import { getSurvey } from '$lib/api';
+import { getSurveyAuth } from '$lib/api';
 import type { PageLoad } from './$types';
+import { jwt } from '../../stores';
+
+export const ssr = false;
 
 export const load = (async ({ fetch, data }) => {
-	return data;
+	const token = jwt.get();
+	console.log("token", token)
+	return {
+		survey: await getSurveyAuth(2, { fetch, token })
+	};
 }) satisfies PageLoad;
