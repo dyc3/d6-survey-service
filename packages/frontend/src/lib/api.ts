03af4ec1f3180cc962d8102723c13f71b81165c6
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-10 14:43:00 +0000

    add token to ExtraOptions

--- packages/frontend/src/lib/api.ts
+++ packages/frontend/src/lib/api.ts
@@ -13,12 +13,13 @@ import type {
 	ValidationError
 } from './common';
 import { jwt } from '../stores';
+import { browser } from '$app/environment';
 
 const API_URL = 'http://localhost:5347'; // TODO: see #42
 
 export type Result<T, E> = { ok: true; value: T } | { ok: false; error: E };
 export type ApiResponse<T> = Result<T, ApiErrorResponse<any>>;
-export type ExtraOptions = { fetch?: typeof fetch };
+export type ExtraOptions = { fetch?: typeof fetch, token?: string };
 
 type ApiRequestOptions = RequestInit & ExtraOptions;
 
@@ -47,7 +48,7 @@ async function apiReq<T>(path: string, options?: ApiRequestOptions): Promise<Api
 }
 
 async function apiReqAuth<T>(path: string, options?: ApiRequestOptions): Promise<ApiResponse<T>> {
-	const token = jwt.get();
+	const token = options?.token ?? (browser ? jwt.get() : undefined);
 	if (!token) {
 		throw new Error(`Not logged in, cannot make authenticated request to ${path}`);
 	}
@@ -87,6 +88,13 @@ export async function getSurvey(
 	opts?: ExtraOptions
 ): Promise<ApiResponse<Survey>> {
 	return apiReq(`/api/survey/${survey_id}`, { ...opts });
+}
+
+export async function getSurveyAuth(
+	survey_id: number,
+	opts?: ExtraOptions
+): Promise<ApiResponse<Survey>> {
+	return apiReqAuth(`/api/survey/${survey_id}`, { ...opts });
 }
 
 export async function createSurvey(opts?: ExtraOptions): Promise<ApiResponse<Survey>> {
