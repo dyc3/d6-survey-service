5447eb06a404158d3a96ad32a13ff41f32b87836
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-10 13:43:46 +0000

    adjust CORS headers

--- crates/api_server/src/cors.rs
+++ crates/api_server/src/cors.rs
@@ -15,11 +15,9 @@ impl Fairing for Cors {
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PATCH, DELETE",
-        ));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "*"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        response.set_header(Header::new("Access-Control-Max-Age", "7200"));
     }
 }
