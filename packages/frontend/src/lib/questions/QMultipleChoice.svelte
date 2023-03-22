9d20e9da61ebaab4fa40e6760d6bb9fe9964be45
Carson McManus <dyc3@users.noreply.github.com>
2023-03-22 15:00:40 -0400

    Make the response page functional (#117)
    - make clicking the submit button functional
    - format and share type
    - response page is now able to construct the right object to submit to
    the server
    - add indicator that response is being submitted
    - fetch response to edit it
    - UI elements actually get populated when you get the survey response
    - fix rating being off by one
    - make patch response endpoint return json
    - remove temp code
    
    does not make validation errors visible
    
    closes #90

--- packages/frontend/src/lib/questions/QMultipleChoice.svelte
+++ packages/frontend/src/lib/questions/QMultipleChoice.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { Choice } from '$lib/common';
+	import type { Choice, Response } from '$lib/common';
 	import Button from '$lib/ui/Button.svelte';
 	import ButtonGroup from '$lib/ui/ButtonGroup.svelte';
 	import TextBox from '$lib/ui/TextBox.svelte';
@@ -24,6 +24,26 @@
 	}
 
 	let group_selected: number | undefined = undefined;
+
+	export let response: Response | undefined = undefined;
+	$: {
+		if (response !== undefined && group_selected === undefined) {
+			if (response.type === 'MultipleChoice') {
+				// TODO: handle multiple selections
+				group_selected = choices.findIndex((choice) => {
+					if (response !== undefined && response.type === 'MultipleChoice') {
+						return response.content.selected.includes(choice.uuid);
+					}
+				});
+			}
+		}
+		if (group_selected !== undefined) {
+			// TODO: handle multiple selections
+			response = { type: 'MultipleChoice', content: { selected: [choices[group_selected].uuid] } };
+		} else {
+			response = undefined;
+		}
+	}
 </script>
 
 <Container>
