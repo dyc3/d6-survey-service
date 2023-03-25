7a87291a7726265420f5788dc4951d2f85e00dc4
Carson McManus <carson.mcmanus1@gmail.com>
2023-03-25 13:35:33 +0000

    add redirect on index page depending on login status

---
+++ packages/frontend/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { jwt } from '../stores';
+	import { goto } from '$app/navigation';
+
+	if (browser) {
+		if (jwt.get() === undefined) {
+			goto('/login');
+		} else {
+			goto('/mysurveys');
+		}
+	}
+</script>
