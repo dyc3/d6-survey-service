c7f7f46d1bac8f382d3d686d1e51fad97c296fcc
Carson McManus <carson.mcmanus1@gmail.com>
2023-04-06 00:38:19 +0000

    fix lints

--- packages/frontend/tests/mysurveys.test.ts
+++ packages/frontend/tests/mysurveys.test.ts
@@ -5,7 +5,6 @@ const username = `testuser-${crypto.randomUUID()}`;
 const password = 'pass';
 
 test.beforeAll(async ({ browser }) => {
-
 	const page = await browser.newPage();
 
 	await page.goto('/login');
@@ -14,10 +13,10 @@ test.beforeAll(async ({ browser }) => {
 	await page.fill('input[name="password"]', password);
 	await page.getByRole('button', { name: 'Submit' }).click();
 	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: 'visible' });
-	let token = await page.evaluate(() => localStorage.getItem('token')) ?? "";
+	const token = (await page.evaluate(() => localStorage.getItem('token'))) ?? '';
 
-	if (token === "") {
-		throw new Error("Token is empty");
+	if (token === '') {
+		throw new Error('Token is empty');
 	}
 });
 
@@ -27,48 +26,48 @@ test.beforeEach(async ({ page }) => {
 	await page.fill('input[name="username"]', username);
 	await page.fill('input[name="password"]', password);
 	await page.getByRole('button', { name: 'Submit' }).click();
-	await page.getByRole("heading", { name: "My Surveys" }).waitFor({ state: "visible" });
+	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: 'visible' });
 });
 
-test("create and delete survey", async ({ page }) => {
-	await page.goto("/mysurveys");
-	await page.getByRole("heading", { name: "My Surveys" }).waitFor({ state: "visible" });
+test('create and delete survey', async ({ page }) => {
+	await page.goto('/mysurveys');
+	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: 'visible' });
 
 	// create survey
 	await page.getByRole('button', { name: 'Create Survey' }).click();
-	await page.getByRole('heading', { name: 'Editing' }).waitFor({ state: "visible" });
+	await page.getByRole('heading', { name: 'Editing' }).waitFor({ state: 'visible' });
 
 	// set the title of the survey
 	await page.getByPlaceholder('Survey Title').click();
 	await page.getByPlaceholder('Survey Title').fill('foo bar');
-	await page.getByRole('heading', { name: 'foo bar' }).waitFor({ state: "visible" });
+	await page.getByRole('heading', { name: 'foo bar' }).waitFor({ state: 'visible' });
 
 	// assert that it saves
-	await page.getByText('Saving...').waitFor({ state: "visible" });
-	await page.getByText('Changes saved').waitFor({ state: "visible" });
+	await page.getByText('Saving...').waitFor({ state: 'visible' });
+	await page.getByText('Changes saved').waitFor({ state: 'visible' });
 
 	// back to mysurveys page
 	await page.goto('/mysurveys');
 
 	// assert that survey with title is present in list
-	await page.getByRole('cell', { name: 'foo bar' }).waitFor({ state: "visible" });
+	await page.getByRole('cell', { name: 'foo bar' }).waitFor({ state: 'visible' });
 
 	// delete survey
 	await page.getByRole('button', { name: 'Delete' }).click();
 
 	// assert that survey is gone
-	expect(await page.locator('cell', { hasText: "foo bar" }).count()).toEqual(0);
+	expect(await page.locator('cell', { hasText: 'foo bar' }).count()).toEqual(0);
 
 	// reload
 	await page.reload();
 
 	// assert that survey is still gone
-	expect(await page.locator('cell', { hasText: "foo bar" }).count()).toEqual(0);
+	expect(await page.locator('cell', { hasText: 'foo bar' }).count()).toEqual(0);
 });
 
-test("create a sruvey with a few questions", async ({ page }) => {
-	await page.goto("/mysurveys");
-	await page.getByRole("heading", { name: "My Surveys" }).waitFor({ state: "visible" });
+test('create a sruvey with a few questions', async ({ page }) => {
+	await page.goto('/mysurveys');
+	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: 'visible' });
 
 	await page.getByRole('button', { name: 'Create Survey' }).click();
 	await page.getByPlaceholder('Survey Title').click();
@@ -95,6 +94,6 @@ test("create a sruvey with a few questions", async ({ page }) => {
 	await page.getByPlaceholder('Insert prompt...').click();
 	await page.getByPlaceholder('Insert prompt...').fill('q3');
 
-	await page.getByText('Saving...').waitFor({ state: "visible" });
-	await page.getByText('Changes saved').waitFor({ state: "visible" });
+	await page.getByText('Saving...').waitFor({ state: 'visible' });
+	await page.getByText('Changes saved').waitFor({ state: 'visible' });
 });
