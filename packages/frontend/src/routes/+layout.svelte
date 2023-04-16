f89a4774c5d8cb7fb7b9317df9ea34d57d700d63
invissy <72313458+invissy@users.noreply.github.com>
2023-04-16 19:20:05 +0000

    add logout button

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	import '$lib/main.scss';
+	import Button from '../lib/ui/Button.svelte';
+	import { jwt } from '../stores';
+	import { goto } from '$app/navigation';
+
+	function logout(){
+		jwt.logout();
+		goto('/login');
+	}
 </script>
 
 <h1>Survey App</h1>
+{#if jwt.get() != undefined}
+<Button on:click={logout}>Log Out</Button>
+{/if}
 <slot />
--- packages/frontend/tests/mysurveys.test.ts
+++ packages/frontend/tests/mysurveys.test.ts
@@ -65,7 +65,7 @@ test('create and delete survey', async ({ page }) => {
 	expect(await page.locator('cell', { hasText: 'foo bar' }).count()).toEqual(0);
 });
 
-test('create a sruvey with a few questions', async ({ page }) => {
+test('create a survey with a few questions', async ({ page }) => {
 	await page.goto('/mysurveys');
 	await page.getByRole('heading', { name: 'My Surveys' }).waitFor({ state: 'visible' });
 
