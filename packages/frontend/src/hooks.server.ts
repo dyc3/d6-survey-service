fb982223a64884118baeb631acc1d97cc33e4dfe
Carson McManus <carson.mcmanus1@gmail.com>
2023-04-19 17:49:21 +0000

    format

--- packages/frontend/src/hooks.server.ts
+++ packages/frontend/src/hooks.server.ts
@@ -1,6 +1,6 @@
 export async function handle({ event, resolve }) {
 	const response = await resolve(event, {
-		filterSerializedResponseHeaders: (name) => true,
+		filterSerializedResponseHeaders: (name) => true
 	});
 
 	return response;
