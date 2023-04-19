5697179005cb36833238b233312dfdfe75931776
invissy <72313458+invissy@users.noreply.github.com>
2023-04-19 23:15:17 +0000

    make logout button appear when logged in

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
 	import '$lib/main.scss';
 	import Button from '../lib/ui/Button.svelte';
+	import { browser } from '$app/environment';
 	import { jwt } from '../stores';
 	import { goto } from '$app/navigation';
 
-	function logout(){
+	let loggedIn = false;
+	if (browser) {
+		if (jwt.get() !== undefined) {
+			loggedIn = true;
+		}
+	}
+
+	function logout() {
 		jwt.logout();
+		loggedIn = false;
 		goto('/login');
 	}
 </script>
 
 <h1>Survey App</h1>
-{#if jwt.get() != undefined}
-<Button on:click={logout}>Log Out</Button>
+{#if loggedIn}
+	<Button on:click={logout}>Log Out</Button>
 {/if}
+
 <slot />
