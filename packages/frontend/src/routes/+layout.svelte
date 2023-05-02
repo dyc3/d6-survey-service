d27fc096d091f64dcbc4e5cb366638cd99ec0a07
Cindy Lee <72313458+invissy@users.noreply.github.com>
2023-05-02 14:20:05 -0400

    Logout button (#177)
    - add logout button to layout.svelte (appears only when logged in)
    - added test (not sure if it works so I commented it out)
    
    closes #157
    
    ---------
    
    Co-authored-by: Carson McManus <carson.mcmanus1@gmail.com>
    Co-authored-by: Nicholas DiMeglio <nickyd528@aol.com>

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 	import '$lib/main.scss';
+	import Button from '../lib/ui/Button.svelte';
+	import { jwt } from '../stores';
+	import { goto } from '$app/navigation';
+
+	let loggedIn = false;
+	jwt.loggedIn.subscribe((value) => {
+		loggedIn = value;
+	});
+
+	function logout() {
+		jwt.logout();
+		goto('/login');
+	}
 </script>
 
 <div class="container">
-	<h1>Survey App</h1>
+	<div class="header-container">
+		<h1>Survey App</h1>
+		{#if loggedIn}
+			<Button on:click={logout}>Log Out</Button>
+		{/if}
+	</div>
+
 	<slot />
 </div>
 
@@ -17,5 +36,11 @@
 		@media screen and (max-width: $page-width) {
 			max-width: 100%;
 		}
+	}
+
+	.header-container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 </style>
