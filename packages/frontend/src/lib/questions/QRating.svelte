fdf9861dc001c78c3b099ad1068bce0e557f0598
Carson McManus <carson.mcmanus1@gmail.com>
2023-05-02 17:18:21 +0000

    align QRating buttons

--- packages/frontend/src/lib/questions/QRating.svelte
+++ packages/frontend/src/lib/questions/QRating.svelte
@@ -93,28 +93,37 @@
 		</div>
 	{/if}
 
-	<div style="width: max-content;">
-		<ButtonGroup
-			orientation="horizontal"
-			size="small"
-			buttons={Array.apply('', Array(max_rating)).map(function (x, i) {
-				return (i + 1).toString();
-			})}
-			forceSelection={false}
-			bind:selected
-		/>
-		<div class="align-rating-text">
-			<span class="description-text">{minText}</span>
-			<span class="description-text">{maxText}</span>
+	<div class="buttons">
+		<div style="width: max-content;">
+			<ButtonGroup
+				orientation="horizontal"
+				size="small"
+				buttons={Array.apply('', Array(max_rating)).map(function (x, i) {
+					return (i + 1).toString();
+				})}
+				forceSelection={false}
+				bind:selected
+			/>
+			<div class="align-rating-text">
+				<span class="description-text">{minText}</span>
+				<span class="description-text">{maxText}</span>
+			</div>
+			{#each validationErrors.get('response') ?? [] as error}
+				<ValidationErrorRenderer {error} />
+			{/each}
 		</div>
-		{#each validationErrors.get('response') ?? [] as error}
-			<ValidationErrorRenderer {error} />
-		{/each}
 	</div>
 </div>
 
 <style lang="scss">
 	@import '../ui/variables';
+
+	.buttons {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
 
 	.text-box-container {
 		display: flex;
