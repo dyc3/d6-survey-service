bd8726d9e76a8772d2fc2b5afd3d80f7ac12d3a7
Carson McManus <carson.mcmanus1@gmail.com>
2023-05-02 17:41:47 +0000

    fix layout on login page

--- packages/frontend/src/routes/login/+page.svelte
+++ packages/frontend/src/routes/login/+page.svelte
@@ -7,6 +7,7 @@
 	import type { UserToken } from '$lib/common';
 	import type { ApiResponse } from '$lib/api';
 	import ButtonGroup from '$lib/ui/ButtonGroup.svelte';
+	import Panel from '$lib/ui/Panel.svelte';
 
 	let username = '';
 	let password = '';
@@ -35,30 +36,32 @@
 	$: tab = selected[0];
 </script>
 
-<div>
-	<ButtonGroup
-		orientation="horizontal"
-		buttons={['Log In', 'Register']}
-		forceSelection={true}
-		bind:selected
-		role="tab"
-	/>
-	{#if tab === 0}
-		<div class="info-container">
-			<TextBox name="username" placeholder="Username" bind:value={username} /> <br />
-			<TextBox name="password" password placeholder="Password" bind:value={password} /> <br />
-			<Button --margin="5px" type="submit" kind="primary" on:click={doLogin}>Submit</Button>
-			<span>{response}</span>
-		</div>
-	{:else if tab === 1}
-		<div class="info-container">
-			<TextBox name="username" placeholder="New Username" bind:value={username} /> <br />
-			<TextBox name="password" password placeholder="New Password" bind:value={password} /> <br />
-			<Button --margin="5px" type="submit" kind="primary" on:click={doRegister}>Submit</Button>
-			<span>{response}</span>
-		</div>
-	{/if}
-</div>
+<Panel border>
+	<div class="flex-center" style="width:fit-content; flex-direction: column; margin: auto">
+		<ButtonGroup
+			orientation="horizontal"
+			buttons={['Log In', 'Register']}
+			forceSelection={true}
+			bind:selected
+			role="tab"
+		/>
+		{#if tab === 0}
+			<div class="info-container">
+				<TextBox name="username" placeholder="Username" bind:value={username} /> <br />
+				<TextBox name="password" password placeholder="Password" bind:value={password} /> <br />
+				<Button --margin="5px" type="submit" kind="primary" on:click={doLogin}>Submit</Button>
+				<span>{response}</span>
+			</div>
+		{:else if tab === 1}
+			<div class="info-container">
+				<TextBox name="username" placeholder="New Username" bind:value={username} /> <br />
+				<TextBox name="password" password placeholder="New Password" bind:value={password} /> <br />
+				<Button --margin="5px" type="submit" kind="primary" on:click={doRegister}>Submit</Button>
+				<span>{response}</span>
+			</div>
+		{/if}
+	</div>
+</Panel>
 
 <style lang="scss">
 	@import '../../lib/ui/variables';
