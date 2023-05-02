d34645fa69f98f713947a425fdd303856c10a552
Carson McManus <carson.mcmanus1@gmail.com>
2023-05-02 15:38:36 +0000

    force all pages to render with a max width

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -2,5 +2,20 @@
 	import '$lib/main.scss';
 </script>
 
-<h1>Survey App</h1>
-<slot />
+<div class="container">
+	<h1>Survey App</h1>
+	<slot />
+</div>
+
+<style lang="scss">
+	$page-width: 1000px;
+
+	.container {
+		max-width: $page-width;
+		margin: 0 auto;
+
+		@media screen and (max-width: $page-width) {
+			max-width: 100%;
+		}
+	}
+</style>
